4d35d0382f0fb6ad3000df4c62dc7cf8cd5a151c
Simon Gatonye <gatonye@trueafrican.com>
2020-05-26 17:07:37 +0300

    ==

--- examples/account_services.go
+++ examples/account_services.go
@@ -11,7 +11,7 @@ func AccountBalance(accessToken, env, countryCode, account, keyPath string) (map
 	if err != nil {
 		log.Printf("%v", err)
 	}
-	accInfo := map[string]interface{
+	accInfo := map[string]interface{} {
 		"countrycode": countryCode,
 		"account": account
 	}
