b48902384b50ca54ded90065e7717a3973b56bad
Simon Gatonye <gatonye@trueafrican.com>
2020-05-26 15:14:52 +0300

    debugging

--- utilities/utilities.go
+++ utilities/utilities.go
@@ -16,19 +16,18 @@ func GenerateToken(apiKey, username, password, baseUrl, path string) *http.Respo
     data := url.Values{}
     data.Set("username", username)
     data.Set("password", password)
-    log.Printf("%v\n", data)
     u, _ := url.ParseRequestURI(baseUrl)
     u.Path = path
     URL := u.String()
-    log.Println(URL)
+    // log.Println(URL)
     client := &http.Client{
 		Timeout: time.Duration(30 * time.Second),
 	}
     r, _ := http.NewRequest("POST", URL, strings.NewReader(data.Encode()))
     r.Header.Add("Authorization", "Basic "+ apiKey)
     r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-    log.Printf("%v\n", r.Header)
+    // log.Printf("%v\n", r.Header)
     resp, _ := client.Do(r)
-	log.Printf("%v\n", resp)
+	// log.Printf("%v\n", resp)
 	return resp
 }
