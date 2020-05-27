605aa81c9a9c7d84dfcb93bb9e32a2f4746256ca
Simon Gatonye <gatonye@trueafrican.com>
2020-05-27 14:52:31 +0300

    ==

--- account_services.go
+++ account_services.go
@@ -2,10 +2,19 @@ package jenga
 
 import (
 	"log"
+
+	"github.com/gats/jenga-go/utilities"
 )
 
 
 func (j *JengaImpl) GetAccountBalance(accInfo map[string]interface{}) {
 	log.Printf("%v\n", j)
 	log.Printf("%v\n", accInfo)
+	data = accInfo["account"]+accInfo["countrycode"]
+	log.Println(data)
+	sig, err := utilities.GenerateSignature(data, j.KeyPath)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	log.Println(sig)
 }
---
+++ utilities/sign_data.go
@@ -0,0 +1,90 @@
+package utilities
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
+func GenerateSignature(data, keypath) (string, error){
+	signer, err := loadPrivateKey(keypath)
+	if err != nil {
+		return "", err
+	}
+	signed, err := signer.Sign([]byte(data))
+	if err != nil {
+		return "", err
+	}
+	sig := base64.StdEncoding.EncodeToString(signed)
+	return sig, nil
+}
+
+
+// loadPrivateKey loads an parses a PEM encoded private key file.
+func loadPrivateKey(path string) (Signer, error) {
+	data, err := ioutil.ReadFile(path)
+    if err != nil {
+        fmt.Println("File reading error", err)
+        return nil, err
+    }
+	return parsePrivateKey([]byte(string(data)))
+}
+
+// parsePublicKey parses a PEM encoded private key.
+func parsePrivateKey(pemBytes []byte) (Signer, error) {
+	block, _ := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, errors.New("ssh: no key found")
+	}
+
+	var rawkey interface{}
+	switch block.Type {
+	case "RSA PRIVATE KEY":
+		rsa, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+		rawkey = rsa
+	default:
+		return nil, fmt.Errorf("ssh: unsupported key type %q", block.Type)
+	}
+	return newSignerFromKey(rawkey)
+}
+
+// A Signer is can create signatures that verify against a public key.
+type Signer interface {
+	// Sign returns raw signature for the given data. This method
+	// will apply the hash specified for the keytype to the data.
+	Sign(data []byte) ([]byte, error)
+}
+
+
+func newSignerFromKey(k interface{}) (Signer, error) {
+	var sshKey Signer
+	switch t := k.(type) {
+	case *rsa.PrivateKey:
+		sshKey = &rsaPrivateKey{t}
+	default:
+		return nil, fmt.Errorf("ssh: unsupported key type %T", k)
+	}
+	return sshKey, nil
+}
+
+type rsaPrivateKey struct {
+	*rsa.PrivateKey
+}
+
+// Sign signs data with rsa-sha256
+func (r *rsaPrivateKey) Sign(data []byte) ([]byte, error) {
+	h := sha256.New()
+	h.Write(data)
+	d := h.Sum(nil)
+	return rsa.SignPKCS1v15(rand.Reader, r.PrivateKey, crypto.SHA256, d)
+}
