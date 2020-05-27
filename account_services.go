6a6eb0f0aa68c76bbc259d22a3aa156cbab84c61
Simon Gatonye <gatonye@trueafrican.com>
2020-05-27 15:15:18 +0300

    ==

--- account_services.go
+++ account_services.go
@@ -2,21 +2,17 @@ package jenga
 
 import (
 	"log"
-
-	// "github.com/gats/jenga-go/utilities"
+	"github.com/gats/jenga-go/utilities"
 )
 
 
 func (j *JengaImpl) GetAccountBalance(accInfo map[string]interface{}) {
 	log.Printf("%v\n", j)
-	// log.Printf("%v\n", accInfo)
-	acc := string(accInfo["account"])
-	cc := string(accInfo["countrycode"])
-	data :=  acc + cc
+	data :=  accInfo["account"].(string) + accInfo["countrycode"].(string)
 	log.Println(data)
-	// sig, err := utilities.GenerateSignature(data, j.KeyPath)
-	// if err != nil {
-	// 	log.Printf("%v", err)
-	// }
-	// log.Println(sig)
+	sig, err := utilities.GenerateSignature(data, j.KeyPath)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	log.Println(sig)
 }
