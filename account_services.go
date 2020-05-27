4d056a60f800990c826c2961b9e89e32bef3511e
Simon Gatonye <gatonye@trueafrican.com>
2020-05-27 15:06:32 +0300

    ==

--- account_services.go
+++ account_services.go
@@ -10,10 +10,10 @@ import (
 func (j *JengaImpl) GetAccountBalance(accInfo map[string]interface{}) {
 	log.Printf("%v\n", j)
 	// log.Printf("%v\n", accInfo)
-	log.Println(accInfo["account"])
-	// cc := accInfo["countrycode"]
-	// data :=  acc + cc
-	// log.Println(data)
+	acc := string(accInfo["account"])
+	cc := string(accInfo["countrycode"])
+	data :=  acc + cc
+	log.Println(data)
 	// sig, err := utilities.GenerateSignature(data, j.KeyPath)
 	// if err != nil {
 	// 	log.Printf("%v", err)
