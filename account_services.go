b4645f9861d36268e1491ba40a70f078753bc4a3
Simon Gatonye <gatonye@trueafrican.com>
2020-05-27 15:51:27 +0300

    added checking of country codes

--- account_services.go
+++ account_services.go
@@ -11,13 +11,12 @@ import (
 func (j *JengaImpl) GetAccountBalance(account, countryCode string) error {
 	ret := utilities.IsAllowedCountryCode(countryCode)
 	if !ret {
-		return errors.New("Country code not allowed")
+		return nil, errors.New("Country code not allowed")
 	}
 	data :=  account + countryCode
 	sig, err := utilities.GenerateSignature(data, j.KeyPath)
 	if err != nil {
-		log.Printf("%v", err)
+		return nil, err
 	}
-	log.Println(sig)
-	return nil
+	return nil,nil
 }
